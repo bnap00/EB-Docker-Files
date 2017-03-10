10c966a405502dbe1e98d8d07c6d42ecb3d0beb1
Bharat <bharat.parsiya@rtcamp.com>
2017-03-10 18:32:51 +0530

    spell check

--- efs/entry.sh
+++ efs/entry.sh
@@ -1,2 +1,2 @@
  mkdir $MOUNT_DIRECTORY
- mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $ILE_SYSTEM_URL $MOUNT_DIRECTORY
+ mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $FILE_SYSTEM_URL $MOUNT_DIRECTORY
