4c576aeb28b408cec49a2c923be151e3fb061a4a
Bharat <bharat.parsiya@rtcamp.com>
2017-03-10 18:23:46 +0530

    test commit

--- efs/entry.sh
+++ efs/entry.sh
@@ -1,6 +1,2 @@
-$fsurl
-$mountpoint = $1
-
-
-mkdir $mountpoint
- mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2  "${fsurl}" "${mountpoint}"
+ mkdir $MOUNT_DIRECTORY
+ mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $ILE_SYSTEM_ID $MOUNT_DIRECTORY
