877106dfb19fea7aed30a28b2e78a264328e8f23
Bharat <bharat.parsiya@rtcamp.com>
2017-03-13 17:26:31 +0530

    remote script change

--- nginx/mountefs.sh
+++ nginx/mountefs.sh
@@ -1,8 +1,49 @@
-if [ -z "$MOUNT_DIRECTORY" ]
-then
-    if [ -z "$FILE_SYSTEM_URL" ]
-    then
-        mkdir -p $MOUNT_DIRECTORY
-        mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $FILE_SYSTEM_URL $MOUNT_DIRECTORY
+#!/bin/bash
+EFS_REGION=$REGION
+EFS_MOUNT_DIR=$MOUNT_DIRECTORY
+EFS_FILE_SYSTEM_ID=$FILE_SYSTEM_ID
+
+echo "Mounting EFS filesystem ${EFS_DNS_NAME} to directory ${EFS_MOUNT_DIR} ..."
+
+echo 'Stopping NFS ID Mapper...'
+service rpcidmapd status &> /dev/null
+if [ $? -ne 0 ] ; then
+    echo 'rpc.idmapd is already stopped!'
+else
+    service rpcidmapd stop
+    if [ $? -ne 0 ] ; then
+        echo 'ERROR: Failed to stop NFS ID Mapper!'
+        exit 1
     fi
 fi
+
+echo 'Checking if EFS mount directory exists...'
+if [ ! -d ${EFS_MOUNT_DIR} ]; then
+    echo "Creating directory ${EFS_MOUNT_DIR} ..."
+    mkdir -p ${EFS_MOUNT_DIR}
+    if [ $? -ne 0 ]; then
+        echo 'ERROR: Directory creation failed!'
+        exit 1
+    fi
+    chmod 777 ${EFS_MOUNT_DIR}
+    if [ $? -ne 0 ]; then
+        echo 'ERROR: Permission update failed!'
+        exit 1
+    fi
+else
+    echo "Directory ${EFS_MOUNT_DIR} already exists!"
+fi
+
+mountpoint -q ${EFS_MOUNT_DIR}
+if [ $? -ne 0 ]; then
+    echo "mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${EFS_FILE_SYSTEM_ID}.efs.${EFS_REGION}.amazonaws.com:/ ${EFS_MOUNT_DIR}"
+    mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${EFS_FILE_SYSTEM_ID}.efs.${EFS_REGION}.amazonaws.com:/ ${EFS_MOUNT_DIR}
+    if [ $? -ne 0 ] ; then
+        echo 'ERROR: Mount command failed!'
+        exit 1
+    fi
+else
+    echo "Directory ${EFS_MOUNT_DIR} is already a valid mountpoint!"
+fi
+
+echo 'EFS mount complete.'
