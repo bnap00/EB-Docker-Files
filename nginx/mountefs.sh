18d21b37199c7a5e16f6c3514e26a59aeaa9f98f
Bharat <bharat.parsiya@rtcamp.com>
2017-03-13 17:12:40 +0530

    add script

--- nginx/mountefs.sh
+++ nginx/mountefs.sh
@@ -1,6 +1,8 @@
-if [[ ! -z $MOUNT_DIRECTORY ]] then
-    if [[ ! -z $FILE_SYSTEM_URL ]] then
-        mkdir $MOUNT_DIRECTORY
+if [ -z "$MOUNT_DIRECTORY" ]
+then
+    if [ -z "$FILE_SYSTEM_URL" ]
+    then
+        mkdir -p $MOUNT_DIRECTORY
         mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $FILE_SYSTEM_URL $MOUNT_DIRECTORY
     fi
-fi 
+fi
